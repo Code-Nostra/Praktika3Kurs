cf43e92dce95f7766887e25ff7ffe25b42597f8c
Code-Nostra <artem2002228228@mail.ru>
2022-03-11 13:03:43 +0300

    Commit

---
+++ CollageProgram/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using CollageProgram.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CollageProgram.Views;
+using System.Xml;
+using System.Windows.Markup;
+
+namespace CollageProgram
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string path;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void _GragPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ CollageProgram/ViewModels/_Pr3ViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CollageProgram.Models;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Input;
+using CollageMvvM.Commands;
+
+namespace CollageProgram.ViewModels
+{
+    public class _Pr3ViewModel : BaseViewModel
+    {
+        #region Смена темы
+        public ObservableCollection<ThemeApplication> ThemeApplication { get; set; }
+
+
+        public ICommand ThemeChangeCommand { get; }
+
+        private void OnThemeChangeCommand(object p)
+        {
+
+            var uri = new Uri(Convert.ToString(p), UriKind.Relative);
+
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+
+            Application.Current.Resources.Clear();
+
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+
+        private bool CanThemeChangeCommand(object p) =>  true;
+        
+        #endregion
+
+
+
+
+        
+        public _Pr3ViewModel()
+        {
+            #region Команды
+
+            ThemeChangeCommand = new LambdaCommand(OnThemeChangeCommand, CanThemeChangeCommand);
+
+            #endregion
+
+            #region Создание новых тем в listview
+            ThemeApplication = new ObservableCollection<ThemeApplication>
+            {
+                new ThemeApplication
+                {
+                    NameTheme="Простая",
+
+                    UrlTheme=@"..\Resources\Themes\DefaultTheme.xaml"
+                },
+                new ThemeApplication
+                {
+                    NameTheme="Стардаст",
+
+                    UrlTheme=@"..\Resources\Themes\StarDustCrussidersTheme.xaml"
+                },
+                 new ThemeApplication
+                {
+                    NameTheme="Каменный океан",
+
+                    UrlTheme=@"..\Resources\Themes\StoneOceanTheme.xaml"
+                }
+
+            };
+            #endregion
+        }
+    }
+}
